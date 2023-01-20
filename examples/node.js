eddd75441ba8790d2f28ef681c24684c406f6d11
Andrej Doubek <doubek.a@gmail.com>
2023-01-20 22:10:29 +0100

    Update example data.

--- examples/node.js
+++ examples/node.js
@@ -1,7 +1,4 @@
-var MPEGParse = require('../parse');
-const url = 'http://demo.castlabs.com/tmp/text0.mp4'
-const bunnyUrl = 'http://techslides.com/demos/sample-videos/small.mp4'
-var parse = new MPEGParse()
-var values = parse.layoutfromURL(bunnyUrl).then(console.log)
-
-
+const MPEGParse = require('../mpeg');
+const url = 'http://techslides.com/demos/sample-videos/small.mp4'
+const parse = new MPEGParse()
+parse.layoutfromURL(url).then(console.log)
